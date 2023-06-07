d0aa4304a756a804a74e6f5ee41755f8328fe89a
Dustin Brown <willCode2Surf@users.noreply.github.com>
2023-06-07 13:04:10 -0500

    expressAppBuildout

--- config/index.js
+++ config/index.js
@@ -7,4 +7,7 @@ export const ConfigurationData = {
   bus: {
     channel: process.env.BUS_CHANNEL || "bf871406-d2fa-4252-9edf-4920924f2a48",
   },
+  server: {
+    port: process.env.SERVER_PORT || 6909
+  }
 };
